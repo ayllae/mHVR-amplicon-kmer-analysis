5c3f892cd629790d94175b0da1adf9d3b82910f0
Aylla von Ermland <ayllaermland@gmail.com>
2026-08-18 13:11:30 -0400

    Update sub.sh

--- Input/sub.sh
+++ Input/sub.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=iob_p 
 #SBATCH --constraint=Genoa
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=ayllae@uga.edu
+#SBATCH --mail-user=user@email.edu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=100gb
@@ -12,14 +12,12 @@
 #SBATCH --error=28.%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-cd /scratch/asv10825/mHVR_June_2026/
+cd /working_directory/raw_files
 
 CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh
-conda activate /home/asv10825/miniconda/envs/kmer_env
+conda activate /miniconda/envs/env
 
-
-# IMPORTANT: Avoid thread oversubscription
 export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export MKL_NUM_THREADS=1
@@ -27,6 +25,6 @@ export NUMEXPR_NUM_THREADS=1
 
 echo "Running with $SLURM_CPUS_PER_TASK CPUs"
 
-
+python 01_cluster_reads.py
 python 02_cluster_visualization_and_summary.py
 
